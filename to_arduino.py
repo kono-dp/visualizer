0a71357c9934e649e4ca271f731d9fb8df71c48e
kono <133991151+kono-dp@users.noreply.github.com>
2024-11-22 02:22:24 +0900

    Add files via upload

--- to_arduino.py
+++ to_arduino.py
@@ -3,7 +3,6 @@ import time
 import serial
 
 
-
 # Pygameを初期化し、ジョイスティック（コントローラー）を設定
 pygame.init()
 pygame.joystick.init()
@@ -11,7 +10,7 @@ num_joysticks = pygame.joystick.get_count()
 print(f"Number of joysticks: {num_joysticks}")
 controller = pygame.joystick.Joystick(0)
 # Arduinoと繋ぐ
-ser = serial.Serial('COM3', 115200) ##################################################################
+ser = serial.Serial('COM4', 115200) ##################################################################
 
 
 button_before = [0] * 16
@@ -36,7 +35,7 @@ try:
                 ZLR[i] = 1
             elif ZLR[i] < -0.5:
                 ZLR[i] = 0
-        
+
         # Arduinoに送る形に整える
         message = [0] * 22
         for i in range(16):
@@ -57,18 +56,21 @@ try:
             else: # ボタンが離された
                 message[i + 20] = 2
             ZLR_before[i] = ZLR[i]
-        
+
         # Arduinoに送る
         message = list(map(str, message))
         message = ",".join(message) + ";"
         print(message)
         # データをバイト列にエンコードして送る
         ser.write(message.encode())
-        
-        # 75Hzで通信する
+
+
+        # {n} Hzで通信する
+        n = 120 #############################################################
         end_time = time.time()
-        time_to_wait = 1 / 120 - (end_time - start_time)  #############################################################
+        time_to_wait = 1 / n - (end_time - start_time)
         time.sleep(time_to_wait)
+
 
 except KeyboardInterrupt:
     pygame.quit()
